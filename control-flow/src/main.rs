f3acac597a0f946bacfe6ce68d2ea354be7bbbf0
Bob-Loth <36461371+Bob-Loth@users.noreply.github.com>
2022-07-09 22:33:40 -0700

    while loop

--- control-flow/src/main.rs
+++ control-flow/src/main.rs
@@ -9,6 +9,14 @@ fn foo(x: i32) -> &'static str {
   return result
 }
 
+fn bar(x: i32) -> &'static str {
+    if x < 10 {
+        "less than 10" //no semicolon indicates return
+    } else {
+        "10 or more"
+    }
+}
+
 fn main() {
     let mut x = 9;
     while x <= 10 {
