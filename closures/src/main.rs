5b50958d68389df7f5e4ab805feb446811bb3845
Bob-Loth <36461371+Bob-Loth@users.noreply.github.com>
2022-07-17 23:42:58 -0700

    more closures examples.

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -1,3 +1,36 @@
+fn closures_1(y: i32) -> impl Fn(i32) -> i32 {
+    //FnOnce is used for functions that should only be called once, FnMut are objects with mutated state.
+    let z = 4;
+    //like closures in other languages, closures capture their enclosing environment (y and z).
+    let f = {
+        move |x: i32| {
+            x + y + z
+        }
+    }; //move is used to copy y and z by value, as they are owned by closures_1 function
+    f
+}
+
+fn add_42(x: i32) -> i64 {
+    x as i64 + 42 //cast x to wider type then add.
+}
+
+fn generics<F>(f: F) -> i64
+    where F: Fn(i32) -> i64
+{
+    f(0)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let y = 6;
+    let f = closures_1(y);
+    let x = 1;
+    println!("{}", f(x));
+
+    let mut v: Vec<i32> = vec![1, 2, 3];
+    v.iter_mut().for_each(|v| *v += f(x));
+    println!("{:#?}", v);
+
+    let a = add_42;
+    println!("{}",generics(a));
+    println!("{}",generics(|x| (x + y).into()));
 }
